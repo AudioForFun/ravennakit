11bfdd50c9b09d00eb3fce1c9c9b8ae819313aac
Ruurd Adema <ruurd@owllab.nl>
2024-10-02 14:36:06 +0200

    Another attempt on fixing Linux compilation

--- src/ravennakit/dnssd/dnssd_browser.cpp
+++ src/ravennakit/dnssd/dnssd_browser.cpp
@@ -13,9 +13,9 @@
 #include "ravennakit/dnssd/bonjour/bonjour_browser.hpp"
 
 std::unique_ptr<rav::dnssd::dnssd_browser> rav::dnssd::dnssd_browser::create() {
-#if defined(RAV_APPLE)
+#if RAV_APPLE
     return std::make_unique<bonjour_browser>();
-#elif defined(RAV_WINDOWS)
+#elif RAV_WINDOWS
     if (dnssd::is_bonjour_service_running()) {
         return std::make_unique<bonjour_browser>();
     } else {
