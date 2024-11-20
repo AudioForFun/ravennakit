296ae865059db8b2fff614e523dfc8baad67b829
Ruurd Adema <ruurd@owllab.nl>
2024-11-20 20:37:42 +0100

    Allow subscriber_list to store additional context

--- include/ravennakit/core/subscriber_list.hpp
+++ include/ravennakit/core/subscriber_list.hpp
@@ -20,8 +20,178 @@ namespace rav {
  * Also, make sure that the subscriber is not destroyed while it is in the list.
  * @tparam T The type of the subscriber.
  */
-template<class T>
+template<class T, class C = void>
 class subscriber_list {
+  public:
+    subscriber_list() = default;
+
+    subscriber_list(const subscriber_list&) = delete;
+    subscriber_list& operator=(const subscriber_list&) = delete;
+
+    subscriber_list(subscriber_list&&) = default;
+    subscriber_list& operator=(subscriber_list&&) = default;
+
+    /**
+     * @returns An iterator to the beginning of the list.
+     */
+    auto begin() const {
+        return subscribers_.begin();
+    }
+
+    /**
+     * @returns An iterator to the end of the list.
+     */
+    auto end() const {
+        return subscribers_.end();
+    }
+
+    /**
+     * @returns An iterator to the beginning of the list.
+     */
+    auto begin() {
+        return subscribers_.begin();
+    }
+
+    /**
+     * @returns An iterator to the end of the list.
+     */
+    auto end() {
+        return subscribers_.end();
+    }
+
+    /**
+     * Adds the given subscriber to the list. If subscriber is already in the list it will not be added and the context
+     * will not be updated. Use add_or_update to update the context.
+     * @param subscriber The subscriber to add.
+     * @param context The context to add.
+     * @return true if the subscriber was added, or false if it was already in the list.
+     */
+    bool add(T* subscriber, C context) {
+        if (contains(subscriber)) {
+            return false;
+        }
+        subscribers_.emplace_back(subscriber, std::move(context));
+        return true;
+    }
+
+    /**
+     * Adds the given subscriber to the list. If subscriber is already in the list it will be updated with the new
+     * context.
+     * @param subscriber The subscriber to add.
+     * @param context The context to add.
+     * @return true if the subscriber was added, or false if it was already in the list. In both cases the context will
+     * be updated.
+     */
+    bool add_or_update_context(T* subscriber, C context) {
+        for (auto& [sub, ctx] : subscribers_) {
+            if (sub == subscriber) {
+                ctx = std::move(context);
+                return false;
+            }
+        }
+        subscribers_.emplace_back(subscriber, std::move(context));
+        return true;
+    }
+
+    /**
+     * Updates the context of the given subscriber.
+     * @param subscriber The subscriber to update.
+     * @param context The new context.
+     * @return true if the subscriber was updated, or false if it was not in the list.
+     */
+    bool update_context(T* subscriber, C context) {
+        for (auto& [sub, ctx] : subscribers_) {
+            if (sub == subscriber) {
+                ctx = std::move(context);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Removes the given subscriber from the list.
+     * @param subscriber The subscriber to remove.
+     * @returns true if the subscriber was removed, or false if it was not in the list.
+     */
+    bool remove(T* subscriber) {
+        for (auto it = subscribers_.begin(); it != subscribers_.end(); ++it) {
+            if (it->first == subscriber) {
+                subscribers_.erase(it);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Clears the list.
+     */
+    void clear() {
+        subscribers_.clear();
+    }
+
+    /**
+     * Calls given function for each subscriber.
+     * @param f The function to call for each subscriber. Must be not null.
+     * @param excluding The subscriber to exclude from the call.
+     */
+    void foreach (const std::function<void(T*, const C&)>& f, const T* excluding = nullptr) {
+        for (auto& [subscriber, ctx] : subscribers_) {
+            if (subscriber != excluding) {
+                f(subscriber, ctx);
+            }
+        }
+    }
+
+    /**
+     * Calls given function for each subscriber.
+     * @param f The function to call for each subscriber. Must be not null.
+     * @param excluding The subscriber to exclude from the call.
+     */
+    void foreach (const std::function<void(T*)>& f, const T* excluding = nullptr) {
+        for (auto& [subscriber, ctx] : subscribers_) {
+            if (subscriber != excluding) {
+                f(subscriber);
+            }
+        }
+    }
+
+    /**
+     * @returns The number of subscribers.
+     */
+    [[nodiscard]] size_t size() const {
+        return subscribers_.size();
+    }
+
+    /**
+     * @return true if there are no subscribers.
+     */
+    [[nodiscard]] bool empty() const {
+        return subscribers_.empty();
+    }
+
+    /**
+     * Checks if the list contains the given subscriber.
+     * @param subscriber The subscriber to check.
+     * @return true if the list contains the subscriber, or false if not.
+     */
+    bool contains(T* subscriber) const {
+        for (auto& [sub, ctx] : subscribers_) {
+            if (sub == subscriber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+  private:
+    std::vector<std::pair<T*, C>> subscribers_;
+};
+
+/// Specialize the void case
+template<class T>
+class subscriber_list<T, void> {
   public:
     subscriber_list() = default;
 
--- test/ravennakit/core/subscriber_list.test.cpp
+++ test/ravennakit/core/subscriber_list.test.cpp
@@ -121,3 +121,102 @@ TEST_CASE("subscriber_list") {
         REQUIRE(list2_subscribers.at(1) == &subscriber2);
     }
 }
+
+TEST_CASE("subscriber_list with context") {
+    rav::subscriber_list<test_subscriber, std::string> list;
+
+    SECTION("Add, notify and remove") {
+        test_subscriber subscriber1;
+        test_subscriber subscriber2;
+        list.add(&subscriber1, "subscriber1");
+        list.add(&subscriber2, "subscriber2");
+
+        list.foreach ([](auto* subscriber, const std::string& ctx) {
+            subscriber->notify(ctx);
+        });
+
+        REQUIRE(subscriber1.messages.size() == 1);
+        REQUIRE(subscriber1.messages[0] == "subscriber1");
+
+        REQUIRE(subscriber2.messages.size() == 1);
+        REQUIRE(subscriber2.messages[0] == "subscriber2");
+
+        list.remove(&subscriber1);
+
+        list.foreach ([](auto* subscriber, const std::string& ctx) {
+            subscriber->notify(ctx);
+        });
+
+        REQUIRE(subscriber1.messages.size() == 1);
+        REQUIRE(subscriber1.messages[0] == "subscriber1");
+
+        REQUIRE(subscriber2.messages.size() == 2);
+        REQUIRE(subscriber2.messages[0] == "subscriber2");
+        REQUIRE(subscriber2.messages[1] == "subscriber2");
+    }
+
+    SECTION("Notify using iterators") {
+        test_subscriber subscriber1;
+        list.add(&subscriber1, "subscriber1");
+
+        for (auto& [sub, ctx] : list) {
+            sub->notify(ctx);
+        }
+
+        REQUIRE(subscriber1.messages.size() == 1);
+        REQUIRE(subscriber1.messages[0] == "subscriber1");
+    }
+
+    SECTION("Double subscribe") {
+        test_subscriber subscriber1;
+        list.add(&subscriber1, "subscriber1-1");
+        REQUIRE(list.size() == 1);
+        list.add(&subscriber1, "subscriber1-2");
+        REQUIRE(list.size() == 1);
+
+        for (auto& [sub, ctx] : list) {
+            sub->notify(ctx);
+        }
+
+        REQUIRE(subscriber1.messages.size() == 1);
+        REQUIRE(subscriber1.messages[0] == "subscriber1-1");
+    }
+
+    SECTION("Move construct") {
+        test_subscriber subscriber1;
+        test_subscriber subscriber2;
+        list.add(&subscriber1, "subscriber1");
+        list.add(&subscriber2, "subscriber2");
+
+        rav::subscriber_list list2(std::move(list));
+
+        REQUIRE(list.empty());
+        REQUIRE(list2.size() == 2);
+    }
+
+    SECTION("Move assign") {
+        test_subscriber subscriber1;
+        test_subscriber subscriber2;
+        list.add(&subscriber1, "subscriber1");
+        list.add(&subscriber2, "subscriber2");
+
+        rav::subscriber_list<test_subscriber, std::string> list2;
+        test_subscriber subscriber3;
+        list2.add(&subscriber3, "subscriber3");
+
+        list2 = std::move(list);
+
+        REQUIRE(list.empty());
+        REQUIRE(list2.size() == 2);
+
+        std::vector<std::pair<test_subscriber*, std::string>> list2_subscribers;
+        for (auto& [sub, ctx] : list2) {
+            list2_subscribers.emplace_back(sub, ctx);
+        }
+
+        REQUIRE(list2_subscribers.at(0).first == &subscriber1);
+        REQUIRE(list2_subscribers.at(0).second == "subscriber1");
+        REQUIRE(list2_subscribers.at(1).first == &subscriber2);
+        REQUIRE(list2_subscribers.at(1).second == "subscriber2");
+    }
+}
